3f5c0810ed3dac529641ee40113423d9b216a278
ouyinheng <2675379992@qq.com>
2019-03-10 20:59:58 +0800

    'Incomplete-initialization'

---
+++ vue.config.js
@@ -0,0 +1,45 @@
+// vue.config.js
+const path = require('path');
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
+module.exports = {
+  // 选项...
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'examples/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
+      // 当使用 title 选项时，
+      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // 在这个页面中包含的块，默认情况下会包含
+      // 提取出来的通用 chunk 和 vendor chunk。
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  },
+  outputDir: '../package',
+  productionSourceMap: false,
+  configureWebpack: {
+    module: {
+      rules:[
+        {
+          test: /\.md$/,
+          loader: 'vue-markdown-loader'
+        }
+      ]
+    }
+  },
+  chainWebpack: (config)=>{
+    config.resolve.alias
+      .set('@', resolve('examples'))
+      .set('assets',resolve('examples/assets'))
+      .set('components',resolve('examples/components'))
+      .set('layout',resolve('examples/layout'))
+      .set('base',resolve('examples/base'))
+      .set('static',resolve('examples/static'))
+  }
+}
